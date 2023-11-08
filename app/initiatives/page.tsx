5330fa137d66bb6ffc602dd09f64398a6b854f60
Elias Simpson <elias@aire.local>
2023-11-08 12:34:12 -0600

    screens changes in tailwind config

--- app/initiatives/page.tsx
+++ app/initiatives/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <SearchBar />
       </Card>
       {/* <TabsContent value="initiatives">*/}
-      <div className="grid grid-flow-row md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 pt-10">
+      <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 pt-10">
         <InitiativeCard />
         <InitiativeCard />
         <InitiativeCard />
