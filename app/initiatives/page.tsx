e9ef2976778ae3805378d48448f381c8421244cb
Elias Simpson <elias@aire.local>
2023-11-08 11:27:28 -0600

    layout change for initiatives page

--- app/initiatives/page.tsx
+++ app/initiatives/page.tsx
@@ -13,13 +13,13 @@ import InitiativeCard from '@/components/initiativeCard';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-stretch container pt-24">
+    <main className="flex min-h-screen flex-col items-stretch px-20 pt-24">
       {/* <Tabs> */}
       <Card className="flex">
         <SearchBar />
       </Card>
       {/* <TabsContent value="initiatives">*/}
-      <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 pt-10">
+      <div className="grid grid-flow-row md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 pt-10">
         <InitiativeCard />
         <InitiativeCard />
         <InitiativeCard />
