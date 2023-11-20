243b51fed11fba38be6245f64ddab879d816b7d1
Kuyawa <kuyawakata@gmail.com>
2023-11-20 19:15:46 -0400

    distinct categories

--- components/OrganizationCard.tsx
+++ components/OrganizationCard.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from 'next/image';
+import Link from 'next/link';
 import { Card, CardContent, CardHeader } from './ui/card';
 import { Progress } from './ui/progress';
 import { Separator } from './ui/separator';
@@ -10,21 +11,26 @@ export default function OrganizationCard({ ...props }) {
   //console.log('ITEM', props?.data)
   const item  = props?.data || {}
   const count = item?.initiative?.length || 0
+  const orgurl = '/organizations/'+item.id
 
   return (
     <Card className="flex flex-col overflow-hidden">
       <CardHeader className="relative h-40 p-3">
-        <Image
-          src={item.image}
-          alt="IMG BG"
-          fill
-          style={{
-            objectFit: 'cover',
-          }}
-        />
+        <Link href={orgurl}>
+          <Image
+            src={item.image}
+            alt="IMG BG"
+            fill
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+        </Link>
       </CardHeader>
       <CardContent className="flex flex-col p-6 pt-3 gap-3">
-        <h3 className="text-2xl font-semibold uppercase">{item.name}</h3>
+        <Link href={orgurl}>
+          <h3 className="text-2xl font-semibold uppercase">{item.name}</h3>
+        </Link>
         <Separator />
         <p>{item.description}</p>
         <ul className="px-3 flex flex-col gap-2">
