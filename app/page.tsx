67c1d008e3c2f9d1301b5b8e82582e77644d6556
Elias Simpson <elias@aire.local>
2023-11-25 14:45:50 -0600

    modal for nft receipt

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,22 @@ import Link from 'next/link'
 import Main from '@/components/ui/main'
 import { Card } from '@/components/ui/card'
 import SearchBar from '@/components/search/SearchBar'
+import NFTReceipt from '@/components/NFTReceipt'
 
-export default function Handler(props:any) {
-  const query    = props?.searchParams?.query || ''
-  const category = props?.searchParams?.category || ''
-  const location = props?.searchParams?.location || ''
-  console.log('SEARCH', query, category, location)
-  return (
-    <Main>
-      <Card className="flex">
-        <SearchBar />
-      </Card>
-      <div className="m-4">
-        Main stuff here...
-      </div>
-    </Main> 
-  )
-} 
+export default function Handler(props: any) {
+     const query = props?.searchParams?.query || ''
+     const category = props?.searchParams?.category || ''
+     const location = props?.searchParams?.location || ''
+     console.log('SEARCH', query, category, location)
+     return (
+          <Main>
+               <Card className="flex">
+                    <SearchBar />
+               </Card>
+               <div className="m-4">
+                    Main stuff here...
+                    <NFTReceipt />
+               </div>
+          </Main>
+     )
+}
