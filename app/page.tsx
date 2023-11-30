73c12cd6ad411920bb7df0a3a7149b06d3363f99
Elias Simpson <elias@aire.local>
2023-11-30 14:34:04 -0600

    setting formatting indent back to 2 spaces to avoid noise for now. grayed out claim button in button component. typo fix.

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,19 @@ import SearchBar from '@/components/search/SearchBar'
 import NFTReceipt from '@/components/NFTReceipt'
 
 export default function Handler(props: any) {
-     const query = props?.searchParams?.query || ''
-     const category = props?.searchParams?.category || ''
-     const location = props?.searchParams?.location || ''
-     console.log('SEARCH', query, category, location)
-     return (
-          <Main>
-               <Card className="flex">
-                    <SearchBar />
-               </Card>
-               <div className="m-4">
-                    Main stuff here...
-                    <NFTReceipt />
-               </div>
-          </Main>
-     )
+  const query = props?.searchParams?.query || ''
+  const category = props?.searchParams?.category || ''
+  const location = props?.searchParams?.location || ''
+  console.log('SEARCH', query, category, location)
+  return (
+    <Main>
+      <Card className="flex">
+        <SearchBar />
+      </Card>
+      <div className="m-4">
+        Main stuff here...
+        <NFTReceipt />
+      </div>
+    </Main>
+  )
 }
